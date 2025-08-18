2b387ef9fd2d3b93ab0c3e03171f6a5249a17764
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-08-18 15:26:35 -0300

    setupExpressServer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-const express = require("express")
-const app = express();
+require("dotenv").config(); 
+const express = require("express");
+const { error } = require("qrcode-terminal");
 
 
-app.get("/health", (req, res)=> {
-    res.status(200).send("healthy");
-})
+class EMTULiveChecker {
+    #app
+    #monitoringIntervals
+    constructor() {
+        this.#app = express(); 
+        this.#monitoringIntervals = new Map();    
+    }
 
-app.listen(3000, ()=> {
-    console.log("We live")
-})
+    async initialize() {
+        try {
+            await this.#setupExpressServer();
+        } catch (ex){
+            console.log("Error on start server, look the error: ", ex);
+            process.exit(1);
+        }
+    }
+    
+    #setupExpressServer() {
+        this.#app.use(express.json());
+
+        this.#app.get("/health", (req, res) => {
+            res.send({
+                status: "running",
+                uptime: process.uptime(),
+                whatsappReady: false,
+                activeMonitors: this.#monitoringIntervals.size(),
+            })
+        });
+
+        const port = process.env.PORT
+
+        if(!port) console.warn("Enviroment variable PORT is not defined, using the 3000 port (default)");
+
+        this.#app.listen(port || 3000, ()=> {
+            console.log(`We are live in port ${ port || 3000 }`);
+        });
+    }
+}
+
+const live_checker = new EMTULiveChecker();
+
+live_checker.initialize();
