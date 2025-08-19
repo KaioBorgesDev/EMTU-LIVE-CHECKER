793175579d55bdb3c393e579df9a432e1a8614a9
KaioBorgesDev <kaioeduardo401@outlook.com>
2025-08-19 08:37:31 -0300

    init messageHandler

--- src/app.js
+++ src/app.js
@@ -1,55 +1,61 @@
-require("dotenv").config(); 
-const express = require("express");
-const Logger = require("./utils/Logger.js");
-const WhatsApp = require("./services/Whatsapp.js")
-const ConfigManager = require("./utils/ConfigManager.js");
+require('dotenv').config(); 
+const express = require('express');
+const Logger = require('./utils/Logger.js');
+const WhatsApp = require('./services/Whatsapp.js');
+const ConfigManager = require('./utils/ConfigManager.js');
+const MessageHandler = require('./services/MessageHandler.js');
+
 
 class EMTULiveChecker {
-    #app
-    #monitoringIntervals
-    #loggs
-    #whatszap
-    #configManager
+    #app;
+    #loggs;
+    #whatszap;
+    #configManager;
+    #messageHandler;
+    #monitoringIntervals;
+
+
     constructor() {
         this.#app = express(); 
         this.#monitoringIntervals = new Map();
         this.#whatszap = new WhatsApp(); 
         this.#configManager = new ConfigManager();
-        this.#loggs = new Logger();   
+        this.#loggs = new Logger();  
+        this.#messageHandler = new MessageHandler();
     }
 
     async initialize() {
         try {
-            this.#loggs.error(`Initializing the server`);
-
             await this.#setupExpressServer();
 
             await this.#whatszap.initialize();
 
-            await this.config
+            await this.#configManager.loadConfigurations();
+
+            await this.#loggs.info('EMTU-LIVE-CHECKER INITIALIZED SUCCESFULLY');
 
         } catch (ex){
-            this.#loggs.error(`Not initialize the server ${error}`);
+            this.#loggs.error(`Not initialize the server ${ex}`);
             process.exit(1);
         }
     }
     
     #setupExpressServer() {
         this.#app.use(express.json());
-        this.#loggs.info("Initializing the server");
+        this.#loggs.info('Initializing the server');
 
-        this.#app.get("/health", (req, res) => {
+        this.#app.get('/health', (req, res) => {
             res.send({
-                status: "running",
+                status: 'running',
                 uptime: process.uptime(),
                 whatsappReady: false,
                 activeMonitors: this.#monitoringIntervals.size(),
-            })
+            });
         });
 
         const port = process.env.PORT;
 
-        if(!port) this.#loggs.warn("Enviroment variable PORT is not defined, using the 3000 port (default)");
+        if(!port) this.#loggs.warn('Enviroment variable PORT is not defined, using the 3000 port (default)');
 
         this.#app.listen(port || 3000, ()=> {
             this.#loggs.info(`We are live in port ${ port || 3000 }`);
